6762cb8d48c545f04ccd6c3c94bdde12b4dfb5c8
Bruce MacLeod <macleod@maine.edu>
2025-02-01 07:08:35 -0500

    Added Config file

--- app.py
+++ app.py
@@ -1,25 +1,28 @@
 import os
 from flask import Flask, request, jsonify
+from config import Config
 
 app = Flask(__name__)
-BASE_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
-SPECIES_DATA_DIR = os.path.join(BASE_DATA_DIR, "mmaforays")
-UPLOADS_DIR = os.path.join(BASE_DATA_DIR, "uploads")
+Config.init_directories()
+
+#BASE_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+#SPECIES_DATA_DIR = os.path.join(BASE_DATA_DIR, "mmaforays")
+#UPLOADS_DIR = os.path.join(BASE_DATA_DIR, "uploads")
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return "Dir : " + SPECIES_DATA_DIR
+    return "Dir : " + Config.SPECIES_DATA_DIR
 
 @app.route("/list_csv_files", methods=["GET"])
 def list_csv_files():
     """List CSV files in a directory."""
-#    directory = request.args.get("directory", "MMAforays")
-#    if directory == "MMAforays":
-    directory_path = SPECIES_DATA_DIR
-#    else:
-#        directory_path = UPLOADS_DIR
-#    directory_path = os.path.join(Config.BASE_DATA_DIR, directory)
+    directory = request.args.get("directory", "mmaforays")
+    if directory == "mmaforays":
+        directory_path = Config.SPECIES_DATA_DIR
+    else:
+        directory_path = Config.UPLOADS_DIR
+    directory_path = os.path.join(Config.BASE_DATA_DIR, directory)
     csv_files = [f for f in os.listdir(directory_path) if f.endswith(".csv")]
     return jsonify({"files": csv_files}), 200
 
---
+++ config.py
@@ -0,0 +1,26 @@
+# config.py
+import os
+from dotenv import load_dotenv
+
+# Load .env file only in local development
+if os.getenv('FLASK_ENV', 'development') == 'development':
+    load_dotenv()
+
+class Config:
+    # Environment
+    ENV = os.getenv('FLASK_ENV', 'development')
+
+    BASE_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+
+    # Subdirectories
+    SPECIES_DATA_DIR = os.path.join(BASE_DATA_DIR, "mmaforays")
+    UPLOADS_DIR = os.path.join(BASE_DATA_DIR, "uploads")
+
+    PRONUNCIATION_CACHE_FILE = os.path.join(BASE_DATA_DIR, "pronounce.csv")
+    INITIAL_FILE_PATH = os.path.join(UPLOADS_DIR, "macleod-obs-taxa.csv")
+
+    @classmethod
+    def init_directories(cls):
+        """Ensure all required directories exist."""
+        os.makedirs(cls.SPECIES_DATA_DIR, exist_ok=True)
+        os.makedirs(cls.UPLOADS_DIR, exist_ok=True)
